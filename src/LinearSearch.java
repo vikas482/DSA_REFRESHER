521d48927625478972a09f9f7f41cc454365711d
VikasMahajan <vikas482@gmail.com>
2022-09-27 15:55:16 +0530

    Added comments

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,4 +1,17 @@
+/*
+Linear Algorithm
+==================
+Linear Search is defined as a sequential search algorithm that starts at one
+end and goes through each element of a list until the desired element is found, otherwise the search continues till the end of the data set. It is the easiest searching algorithm.
+Time Complexity  - How time grows as input grows
+Best Case - O(1)
+Worst Case - O(N) whereas N is Size of Array
+Space Complexity - How much space grows as input grows
+O(1) -  means just 1 variable/ array is used, so it is O(1) or Constant space complexity.
+*/
+
 public class LinearSearch {
+
 
     public static void main(String args[]){
 
